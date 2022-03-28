313ee8c9e0bd857af976e0347fe924c4d9bd0b41
JosephJoshua <jj.anggita@gmail.com>
2022-03-28 17:00:49 +0700

    Add confirmation dialog before generating example sentences

--- bulk_sentences.py
+++ bulk_sentences.py
@@ -1,6 +1,6 @@
 from typing import Sequence
 
-from aqt import Collection, mw
+from aqt import Collection
 from aqt.browser import Browser
 from aqt.operations import CollectionOp
 from aqt.qt import *
@@ -10,13 +10,47 @@ from .example_sentences import fill_note
 
 ACTION_NAME = 'Bulk-add Example Sentences'
 
+def show_confirmation_dialog(note_count: int, browser: Browser):
+    reply = QMessageBox.question(browser, ACTION_NAME, f'Are you sure you want to generate example sentences for {note_count} notes?')
+    return reply == QMessageBox.StandardButton.Yes
+
+def show_success_dialog():
+    dialog = QDialog()
+
+    # Layout
+    layout = QVBoxLayout()
+
+    layout.setContentsMargins(16, 16, 16, 16)
+    layout.setSpacing(8)
+
+    # Message
+    message = QLabel()
+    message.setText(f'Succesfully added example sentences for {note_count} notes')
+
+    layout.addWidget(message)
+
+    # Button box
+    button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
+    qconnect(button_box.accepted, lambda: dialog.reject())
+
+    layout.addWidget(button_box)
+
+    # Window icon
+    icon = QIcon()
+    icon.addPixmap(QPixmap(":/icons/anki.png"), QIcon.Normal, QIcon.Off)
+
+    dialog.setWindowTitle(ACTION_NAME)
+    dialog.setWindowIcon(icon)
+    dialog.setLayout(layout)
+    dialog.exec()
+
 def generate_sentences(selected_nids: Sequence, browser: Browser):
     note_count = len(selected_nids)
+    if not show_confirmation_dialog(note_count, browser):
+        return
 
     def do(col: Collection):
         changed_notes = []
-
-        note_count = len(selected_nids)
         note_index = 1
 
         for nid in selected_nids:
@@ -30,7 +64,7 @@ def generate_sentences(selected_nids: Sequence, browser: Browser):
             aqt.mw.taskman.run_on_main(
                 lambda: aqt.mw.progress.update(
                     label=f"{note['Front']} ({note_index}/{note_count})",
-                    value=note_index - 1,
+                    value=note_index,
                     max=note_count
                 )
             )
@@ -39,17 +73,8 @@ def generate_sentences(selected_nids: Sequence, browser: Browser):
 
         return col.update_notes(changed_notes)
 
-    def show_success_dialog(_):
-        dialog = QMessageBox()
-
-        dialog.setContentsMargins(0, 5, 5, 5)
-        dialog.setWindowTitle(ACTION_NAME)
-        dialog.setText(f'Succesfully added example sentences for {note_count} notes')
-
-        dialog.exec()
-
     operation = CollectionOp(parent=browser, op=do)
-    operation.success(show_success_dialog)
+    operation.success(lambda _: show_success_dialog())
     operation.run_in_background()
 
 def setup_menu_items(browser: Browser):
